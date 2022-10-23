e3ad071256884d7513060b348fcadc6c808e781a
FEDEFED <47941652+FEDEFED@users.noreply.github.com>
2022-10-23 15:38:45 +0200

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,1 +1,12 @@
-
+print('Not in main')
+def main():
+    connect()
+    for i in range(20):
+        print('Hi'+str(i))
+    setseconds(True)
+    for i in range(20):
+        print('Hi'+str(i))
+    setclock(False)
+    for i in range(20):
+        print('Hi'+str(i))
+    
